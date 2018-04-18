7c040b332e1eea023627f2e7189b15675aea93d5
dosssman <dosssman@hotmail.fr>
2018-04-18 13:07:07 +0900

    Added 第二回のTask1 and Task2

---
+++ threejs/sandbox.js
@@ -0,0 +1,42 @@
+function main() {
+  var width = 400;
+  var height = 400;
+
+  var scene = new THREE.Scene();
+
+  var fov = 45;
+  var aspect = width / height;
+  var near = 1;
+  var far = 1000;
+
+  var camera = new THREE.PerspectiveCamera( fov, aspect, near, far);
+  camera.position.set( 0,0,5);
+  scene.add( camera);
+
+  var renderer = new THREE.WebGLRenderer( { depth: true});
+  renderer.setSize( width, height);
+
+  document.body.appendChild( renderer.domElement );
+
+
+  var light = new THREE.PointLight( 0xffffff);
+  light.position.set( 0,0,5);
+  scene.add( light);
+
+  var geometry = new THREE.CubeGeometry( 1,1,1);
+
+  var material = new THREE.MeshLambertMaterial( { color: "blue"});
+  var cube = new THREE.Mesh( geometry, material);
+  scene.add( cube);
+
+  loop();
+
+  function loop() {
+    requestAnimationFrame( loop);
+    cube.rotation.x += .01;
+    cube.rotation.y += .01;
+    renderer.render( scene, camera);
+  }
+}
+
+main();
